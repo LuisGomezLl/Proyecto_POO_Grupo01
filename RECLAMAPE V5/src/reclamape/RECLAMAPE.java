db45984d874a3e23198f3f9801d9a02d6d5152dd
BryanLluncor <129539119+BryanLluncor@users.noreply.github.com>
2023-05-21 11:43:16 -0500

    Realizacion de Interfaces Graficas/Clases Project
    En este punto empezamos a diseñar las interfaces gráficas basándonos en el mockup realizado con la herramienta Firma,
    también se implementó las clases del proyecto
    https://www.figma.com/file/tq8trSk4W5bgzmMKowMqD1/RECLAMAPE?type=design&node-id=0-1&t=iGNvUISSgWF9FEI5-0

---
+++ RECLAMAPE V5/src/reclamape/RECLAMAPE.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reclamape;
+
+
+
+/**
+ *
+ * @author RYZEN3
+ */
+public class RECLAMAPE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         //Incio la ventana principal de gestor de cita
+        JFrame_MenuInicio menu_principal =new JFrame_MenuInicio();  //llamar la ventana princal
+        menu_principal.setLocationRelativeTo(null); //Para centrar en la ventana 
+        menu_principal.setVisible(true); //Hacer visible el formulario
+        
+        // TODO code application logic here
+    }
+    
+}
