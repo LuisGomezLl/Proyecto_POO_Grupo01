6a90beec3e16d9caabc8742985b3dd6d7fb36b71
LuzHuarcaya <129538939+LuzHuarcaya@users.noreply.github.com>
2023-06-08 13:43:32 -0500

    Implementación del Login
    En esta parte, se empieza a implementar el login, haciendo uso del programa Xampp que nos permite acceder a PHP y de esta manera poder relacionar nuestro programa con una base de datos para el registro de usuarios.

---
+++ RECLAMAPE V7/src/Entidades/Reclamo.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author brall
+ */
+public class Reclamo {
+    private String asunto;
+    private String descripcion;
+
+    /**
+     * @return the asunto
+     */
+    public String getAsunto() {
+        return asunto;
+    }
+
+    /**
+     * @param asunto the asunto to set
+     */
+    public void setAsunto(String asunto) {
+        this.asunto = asunto;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+}
